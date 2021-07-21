8697b9aa60c150154137602b73b2b00e6714824e
myan-ish <ranaxmond@gmail.com>
2021-07-21 19:40:23 +0545

    model

---
+++ lib/models/callLog2.dart
@@ -0,0 +1,18 @@
+class CallLog2 {
+  String name;
+  int number;
+  String type;
+  DateTime duration;
+  DateTime date;
+  int accountID;
+  String sim;
+
+  CallLog2(
+      {required this.name,
+      required this.number,
+      required this.type,
+      required this.duration,
+      required this.date,
+      required this.accountID,
+      required this.sim});
+}
